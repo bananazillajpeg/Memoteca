704c6c096be9e23cbad4d868df094a35eca355e8
bananazillajpeg <156859968+bananazillajpeg@users.noreply.github.com>
2024-09-17 17:26:09 -0300

    Update main.js

--- Memoteca/js/main.js
+++ Memoteca/js/main.js
@@ -1,5 +1,34 @@
-import ui from './ui.js'
+import ui from "./ui.js"
+import api from "./api.js"
 
-document.addEventListener('DOMContentLoaded', () => {
+async function manipularSubmissaoFormulario(event) {
+    event.preventDefault()
+    console.log('oi')
+    const id = document.getElementById("pensamento-id").value
+    const conteudo = document.getElementById("pensamento-conteudo").value
+    const autoria = document.getElementById("pensamento-autoria").value
+    console.log('ola')
+    try {
+        await api.salvarPensamento({ conteudo, autoria })
+        ui.renderizarPensamentos()
+    }
+    catch {
+        alert("Erro ao salvar pensamento")
+    }
+
+    function manipularCancelamento() {
+        ui.limparFormulario();
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
     ui.renderizarPensamentos()
+
+    const formularioPensamento = document.getElementById("pensamento-form")
+    const btnCancelar = document.getElementById("botao-cancelar")
+
+    console.log('hi')
+    formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
+    btnCancelar.addEventListener("click", manipularCancelamento)
+
 })
