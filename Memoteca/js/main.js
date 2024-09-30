aea07cdc9f275eff5bcb1e995235b77bd251dfea
bananazillajpeg <156859968+bananazillajpeg@users.noreply.github.com>
2024-09-30 17:39:42 -0300

    Update main.js

--- Memoteca/js/main.js
+++ Memoteca/js/main.js
@@ -3,22 +3,26 @@ import api from "./api.js"
 
 async function manipularSubmissaoFormulario(event) {
     event.preventDefault()
-    console.log('oi')
+
     const id = document.getElementById("pensamento-id").value
     const conteudo = document.getElementById("pensamento-conteudo").value
     const autoria = document.getElementById("pensamento-autoria").value
-    console.log('ola')
+
     try {
-        await api.salvarPensamento({ conteudo, autoria })
+        if (id) {
+            await api.editarPensamento({ id, conteudo, autoria })
+        } else {
+            await api.salvarPensamento({ conteudo, autoria })
+        }
         ui.renderizarPensamentos()
     }
     catch {
         alert("Erro ao salvar pensamento")
     }
+}
 
-    function manipularCancelamento() {
-        ui.limparFormulario();
-    }
+function manipularCancelamento() {
+    ui.limparFormulario();
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -27,7 +31,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const formularioPensamento = document.getElementById("pensamento-form")
     const btnCancelar = document.getElementById("botao-cancelar")
 
-    console.log('hi')
+
     formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
     btnCancelar.addEventListener("click", manipularCancelamento)
 
