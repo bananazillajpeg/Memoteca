334ba46f2109030f4e412c31055bed8b32823608
bananazillajpeg <156859968+bananazillajpeg@users.noreply.github.com>
2024-10-22 22:28:26 -0300

    Update main.js

--- Memoteca/js/main.js
+++ Memoteca/js/main.js
@@ -1,38 +1,105 @@
 import ui from "./ui.js"
 import api from "./api.js"
 
-async function manipularSubmissaoFormulario(event) {
-    event.preventDefault()
+const pensamentosSet = new Set()
 
-    const id = document.getElementById("pensamento-id").value
-    const conteudo = document.getElementById("pensamento-conteudo").value
-    const autoria = document.getElementById("pensamento-autoria").value
-
-    try {
-        if (id) {
-            await api.editarPensamento({ id, conteudo, autoria })
-        } else {
-            await api.salvarPensamento({ conteudo, autoria })
-        }
-        ui.renderizarPensamentos()
-    }
-    catch {
-        alert("Erro ao salvar pensamento")
-    }
+async function adicionarChaveAoPensamento() {
+  try {
+    const pensamentos = await api.buscarPensamentos()
+    pensamentos.forEach(pensamento => {
+      const chavePensamento = `${pensamento.conteudo.trim().toLowerCase()}-${pensamento.autoria.trim().toLowerCase()}`
+      pensamentosSet.add(chavePensamento)
+    });
+  } catch (error) {
+    alert("Erro ao adicionar chave ao pensamento")
+  }
 }
 
-function manipularCancelamento() {
-    ui.limparFormulario();
+function removerEspacos(string) {
+  return string.replaceAll(/\s+/g, '')
+}
+
+const regexConteudo = /^[A-Za-z\s]{10,}$/
+const regexAutoria = /^[A-Za-z]{3,15}$/
+
+function validarConteudo(conteudo) {
+  return regexConteudo.test(conteudo)
+}
+function validarAutoria(autoria) {
+  return regexAutoria.test(autoria)
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    ui.renderizarPensamentos()
+  ui.renderizarPensamentos()
+  adicionarChaveAoPensamento()
 
-    const formularioPensamento = document.getElementById("pensamento-form")
-    const btnCancelar = document.getElementById("botao-cancelar")
+  const formularioPensamento = document.getElementById("pensamento-form")
+  const botaoCancelar = document.getElementById("botao-cancelar")
+  const inputBusca = document.getElementById("campo-busca")
 
-
-    formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
-    btnCancelar.addEventListener("click", manipularCancelamento)
-
+  formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
+  botaoCancelar.addEventListener("click", manipularCancelamento)
+  inputBusca.addEventListener("input", manipularBusca)
 })
+
+async function manipularSubmissaoFormulario(event) {
+  event.preventDefault()
+  const id = document.getElementById("pensamento-id").value
+  const conteudo = document.getElementById("pensamento-conteudo").value
+  const autoria = document.getElementById("pensamento-autoria").value
+  const data = document.getElementById("pensamento-data").value
+
+  const conteudoSemEspacos = removerEspacos(conteudo)
+  const autoriaSemEspacos = removerEspacos(autoria)
+
+  if (!validarConteudo(conteudoSemEspacos)) {
+    alert("É permitido a inclusão apenas de letras e espaços e o minimo de 10 caracteres")
+    return
+  }
+  if (!validarAutoria(autoriaSemEspacos)) {
+    alert("A autoria não pode conter espaços e exige entre três a quinze caracteres")
+    return
+  }
+
+  if (!validarData(data)) {
+    alert("Não é permitido o cadastro de datas futuras. Selecione outra data.")
+  }
+
+  const chaveNovoPensamento = `${conteudo.trim().toLowerCase()}-${autoria.trim().toLowerCase()}`
+
+  if (pensamentosSet.has(chaveNovoPensamento)) {
+    alert("Esse pensamento já existe")
+    return
+  }
+
+  try {
+    if (id) {
+      await api.editarPensamento({ id, conteudo, autoria, data })
+    } else {
+      await api.salvarPensamento({ conteudo, autoria, data })
+    }
+    ui.renderizarPensamentos()
+  } catch {
+    alert("Erro ao salvar pensamento")
+  }
+}
+
+function manipularCancelamento() {
+  ui.limparFormulario()
+}
+
+async function manipularBusca() {
+  const termoBusca = document.getElementById("campo-busca").value
+  try {
+    const pensamentosFiltrados = await api.buscarPensamentoPorTermo(termoBusca)
+    ui.renderizarPensamentos(pensamentosFiltrados)
+  } catch (error) {
+    alert("Erro ao realizar busca")
+  }
+}
+
+function validarData(data) {
+  const dataAtual = new Date()
+  const dataInserida = new Date(data)
+  return dataInserida <= dataAtual
+}
